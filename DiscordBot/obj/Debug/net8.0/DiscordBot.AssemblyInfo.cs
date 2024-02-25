086618911e76a7a66788419e860e1a7cbe5ba9bd
NicoRugani <159579130+NicoRugani@users.noreply.github.com>
2024-02-25 13:45:37 -0600

    Testing

--- DiscordBot/obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
+++ DiscordBot/obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed7ccb00bb0b4552b9a3689655a10eb45749240c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d7cba7fb1bb5cd15228c14a0baa5a6fff58e67")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
